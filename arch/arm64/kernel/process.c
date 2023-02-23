msfjarvis/wahoo

arm64: Make __stack_chk_guard visible
When LTO is used, GCC optimizes out the __stack_chk_guard variable,
resulting in undefined symbol errors. Fix it by explicitly making
__stack_chk_guard visible so it doesn't disappear.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

arch/arm64/kernel/process.c
@@ -59,7 +59,7 @@
 
 #ifdef CONFIG_CC_STACKPROTECTOR
 #include <linux/stackprotector.h>
-unsigned long __stack_chk_guard __read_mostly;
+__visible unsigned long __stack_chk_guard __read_mostly;
 EXPORT_SYMBOL(__stack_chk_guard);
 #endif
 

kvanbiesen/android_kernel_xiaomi_southwest-4.19

Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
Signed-off-by: clarencelol <clarencekuiek@icloud.com>
Signed-off-by: pix106 <sbordenave@gmail.com>

arch/arm64/kernel/process.c
@@ -64,7 +64,7 @@
 
 #ifdef CONFIG_STACKPROTECTOR
 #include <linux/stackprotector.h>
-unsigned long __stack_chk_guard __ro_after_init;
+__visible unsigned long __stack_chk_guard __ro_after_init;
 EXPORT_SYMBOL(__stack_chk_guard);
 #endif
 